6be3662ceefc4b752a4afd378821f2fa728c8220
Hilda Sofia Rada Beleño <radahildasofia@gmail.com>
2026-03-12 21:20:59 -0500

    Add mysql_config method to settings class
    Added a static method to retrieve MySQL configuration from environment variables.

--- config.py
+++ config.py
@@ -42,5 +42,17 @@ class Settings:
             "database": self.db_name,
         }
 
+class settings:
+
+    @staticmethod
+    def mysql_config():
+        return {
+            "host": os.getenv("DB_HOST"),
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "database": os.getenv("DB_NAME"),
+            "port": int(os.getenv("DB_PORT"))
+        }
+
 
 settings = Settings()
